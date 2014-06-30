b2b9ba5ad3628805d472659cd0279815770d77c3
asanctor <asanctor@vub.ac.be>
2014-06-30 15:30:19 +0200

    hfgf

--- app/src/main/java/be/vub/smappeerules/MyActivity.java
+++ app/src/main/java/be/vub/smappeerules/MyActivity.java
@@ -17,7 +17,7 @@ public class MyActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present.hgfgf
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
     }
